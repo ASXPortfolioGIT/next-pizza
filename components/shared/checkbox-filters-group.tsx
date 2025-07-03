be295c09aa3b53456333947eff8bb9d510f92cda
Alexandr Xoxon <4218820@gmail.com>
2025-07-03 10:41:02 +0300

    добавлен скелетон на список ингридиентов

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -3,6 +3,7 @@
 
 import React from 'react'
 import { Input } from '../ui/input'
+import { Skeleton } from '../ui/skeleton'
 import { FilterCheckbox, FilterCheckboxProps } from './filter-checkbox'
 
 type Item = FilterCheckboxProps;
@@ -12,6 +13,7 @@ interface Props {
   items: Item[];
   defaultItems: Item[];
   limit?: number;
+  loading?: boolean;
   searchInputPlaceholder?: string;
   onChange?: (values: string[]) => void;
   defaultValue?: string[];
@@ -25,6 +27,7 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
   limit = 5,
   searchInputPlaceholder = 'Поиск...',
   className,
+  loading,
   onChange,
   defaultValue,
 }) => {
@@ -38,6 +41,17 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
     setSearchValue(e.target.value);
   };
 
+  if (loading) {
+    return <div className={className}>
+      <p className='font-bold mb-3'>{title}</p>
+      {
+        ...Array(limit).fill(0).map((_, index) => (
+          <Skeleton key={index} className="h-6 mb-4 rounded-[8px]" />
+        ))
+      }
+      <Skeleton className="w-28 h-6 mb-4 rounded-[8px]" />
+    </div>
+  }
 
    // обработчик изменения чекбокса
   // при изменении чекбокса вызываем onChange, если он передан
