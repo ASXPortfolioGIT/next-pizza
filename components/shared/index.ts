481c463e7dee2122f76efd0c255bb309da0acb29
Alexandr Xoxon <4218820@gmail.com>
2025-07-11 20:52:26 +0300

    настроено окно отображения товара + правая  группа

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,12 +1,14 @@
-
-export { Categories } from './categories'
-export { CheckboxFiltersGroup } from './checkbox-filters-group'
-export { Container } from './container'
-export { FilterCheckbox } from './filter-checkbox'
-export { Filters } from './filters'
-export { Header } from './header'
-export { RangeSlider } from './range-slider'
-export { SearchInput } from './search-input'
-export { SortPopup } from './sort-popup'
-export { Title } from './title'
-export { TopBar } from './top-bar'
+export { Categories } from "./categories";
+export { CheckboxFiltersGroup } from "./checkbox-filters-group";
+export { Container } from "./container";
+export { FilterCheckbox } from "./filter-checkbox";
+export { Filters } from "./filters";
+export { GroupVariants } from "./group-variants";
+export { Header } from "./header";
+export { ProductCard } from "./product-card";
+export { ProductImage } from "./product-image";
+export { RangeSlider } from "./range-slider";
+export { SearchInput } from "./search-input";
+export { SortPopup } from "./sort-popup";
+export { Title } from "./title";
+export { TopBar } from "./top-bar";
